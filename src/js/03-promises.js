dc790af78e3d24cc1d4429ed14ed04c3cc864243
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-05-21 09:30:12 +0300

    ygky

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+
+const form = document.querySelector(`.form`);
+console.log(form)
+
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+ setTimeout(()=>{
+    if (shouldResolve) {
+    resolve({position, delay});
   } else {
-    // Reject
+    reject({position, delay});
   }
+ } , delay);
+});
+};
+
+form.addEventListener(`submit`, (e)=>{
+e.preventDefault();
+const delay = form.delay.value;
+const step       =  form.step.value;
+const amount     = form.amount.value;
+let numDelay = delay;
+if (numDelay <= 0) {
+  return;
 }
+for (let i = 0; i < amount; i += 1) {
+  let numPosition = i + 1;
+  createPromise(numPosition, numDelay)
+
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  numDelay += step;
+}
+
+
+
+})
+
+
+
+
+
+
+
